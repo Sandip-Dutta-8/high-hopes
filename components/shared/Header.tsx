daff915b4c82219a6f7cc10b3d8591eb5453f3f5
Sandip_Dutta_8 <ayandutta2005@gmail.com>
2024-04-23 01:12:18 +0530

    header updated with authentication

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,20 +1,41 @@
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { Button } from '../ui/button'
+import NavItems from './NavItems'
+import MobileNav from './MobileNav'
 
 const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
-        <Link href="/" className="w-36">
-          <Image 
-            src="/assets/images/logo.svg" width={128} height={38}
-            alt="Evently logo" 
+        <Link href="/" className="w-36 flex items-center">
+          <Image
+            src="/assets/images/logo1.png" width={38} height={20}
+            alt="Evently logo"
           />
+          <h1 className='font-bold text-2xl lg:pl-3 pl-2'>Deadlines</h1>
         </Link>
 
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
         <div className="flex w-32 justify-end gap-3">
-          
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">
+                Login
+              </Link>
+            </Button>
+          </SignedOut>
         </div>
       </div>
     </header>
---
+++ components/shared/MobileNav.tsx
@@ -0,0 +1,40 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import Image from "next/image"
+import { Separator } from "../ui/separator"
+import NavItems from "./NavItems"
+
+
+const MobileNav = () => {
+  return (
+    <nav className="md:hidden">
+      <Sheet>
+        <SheetTrigger className="align-middle">
+          <Image
+            src="/assets/icons/menu.svg"
+            alt="menu"
+            width={24}
+            height={24}
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+          <div className="w-36 flex items-center">
+            <Image
+              src="/assets/images/logo1.png" width={38} height={20}
+              alt="Evently logo"
+            />
+            <h1 className='font-bold text-2xl lg:pl-3 pl-2'>Deadlines</h1>
+          </div>
+          <Separator className="border border-gray-50" />
+          <NavItems />
+        </SheetContent>
+      </Sheet>
+    </nav>
+  )
+}
+
+export default MobileNav
